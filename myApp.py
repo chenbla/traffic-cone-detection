c131c8b38f23c30b1d063f30aa3d7a33596529df
porteon <mustafa.ezer@gazi.edu.tr>
2018-04-22 02:28:41 +0300

    myApp.py, Commit No: 2

--- myApp.py
+++ myApp.py
@@ -1,28 +1,27 @@
 from Tkinter import *
 import tkFileDialog
-import cv2
-import numpy as np
+from PIL import Image, ImageTk
 
 master = Tk()
+master.title("GUI")
 master.geometry("1280x720+500+500")
 master.resizable(0, 0)
 
-c = Canvas(master, bg="blue", height=720, width=720)
-c.pack()
-
-img = ImageTk.PhotoImage(Image.open(path))
-panel = Label(master, image = img)
-panel.pack(side="bottom", fill="both", expand="yes")
-
 def callback():
-    path = tkFileDialog.askopenfilename()
+    path = tkFileDialog.askopenfilename(
+        initialdir="/home/porteon/Documents/Tez/Images/")
     e.delete(0, END)  # Remove current text in entry
     e.insert(0, path)  # Insert the 'path'
-
 
 w = Label(master, text="File Path:")
 e = Entry(master, text="")
 b = Button(master, text="Browse", command=callback)
+
+im = Image.open("/home/porteon/Documents/Tez/Images/test1.jpeg")
+photo = ImageTk.PhotoImage(im)
+cv = Canvas()
+cv.pack(side="top", fill="both", expand="yes")
+cv.create_image(10, 10, image=photo, anchor="nw")
 
 w.pack(side=TOP)
 e.pack(side=TOP)
