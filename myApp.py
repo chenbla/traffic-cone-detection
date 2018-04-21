033cc16402497b35b95be42bc64f709a72df887d
porteon <mustafa.ezer@gazi.edu.tr>
2018-04-22 01:15:44 +0300

    Initial commit of myApp.py

---
+++ myApp.py
@@ -0,0 +1,27 @@
+from Tkinter import *
+import tkFileDialog
+import cv2
+import numpy as np
+
+master = Tk()
+master.geometry("1280x720+500+500")
+master.resizable(0, 0)
+
+c = Canvas(master, bg="blue", height=720, width=720)
+c.pack()
+
+def callback():
+    path = tkFileDialog.askopenfilename()
+    e.delete(0, END)  # Remove current text in entry
+    e.insert(0, path)  # Insert the 'path'
+
+
+w = Label(master, text="File Path:")
+e = Entry(master, text="")
+b = Button(master, text="Browse", command=callback)
+
+w.pack(side=TOP)
+e.pack(side=TOP)
+b.pack(side=TOP)
+
+master.mainloop()
