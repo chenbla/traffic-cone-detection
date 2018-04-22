6a32c1eaeb1e2e59aa2a525803b49d5671880b03
porteon <mustafa.ezer@gazi.edu.tr>
2018-04-22 16:08:00 +0300

    myApp.py, Commit No: 3

--- myApp.py
+++ myApp.py
@@ -1,30 +1,39 @@
 from Tkinter import *
 import tkFileDialog
 from PIL import Image, ImageTk
+import cv2
+import numpy as np
 
 master = Tk()
 master.title("GUI")
 master.geometry("1280x720+500+500")
 master.resizable(0, 0)
 
+canvas = Canvas(width=300, height=200, bg='black')
+canvas.pack(expand=YES, fill=BOTH)
+
+path = ''
+
 def callback():
+
     path = tkFileDialog.askopenfilename(
         initialdir="/home/porteon/Documents/Tez/Images/")
     e.delete(0, END)  # Remove current text in entry
     e.insert(0, path)  # Insert the 'path'
 
-w = Label(master, text="File Path:")
-e = Entry(master, text="")
-b = Button(master, text="Browse", command=callback)
+    img = PhotoImage(file=path)
+    canvas.create_image(50, 10, image=img, anchor=NW)
 
-im = Image.open(FPATH.read())
-photo = ImageTk.PhotoImage(im)
-cv = Canvas()
-cv.pack(side="top", fill="both", expand="yes")
-cv.create_image(10, 10, image=photo, anchor="nw")
+if __name__ == "__main__":
 
-w.pack(side=TOP)
-e.pack(side=TOP)
-b.pack(side=TOP)
+    w = Label(master, text="File Path:")
+    e = Entry(master, text="")
+    b = Button(master, text="Browse", command=callback)
 
-master.mainloop()
+
+    w.pack(side=TOP)
+    e.pack(side=TOP)
+    b.pack(side=TOP)
+    # lbl.pack()
+
+    master.mainloop()
